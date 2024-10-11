0bfe57503e95d53113c0c31e3e686c5334776d68
Ella Lowe-Ng <ella.lowe-ng@smus.ca>
2024-10-10 23:51:24 -0700

    Added specification for the HealthMetric class

---
+++ src/main/model/HealthMetric.java
@@ -0,0 +1,29 @@
+package model;
+
+import java.util.List;
+
+public class HealthMetric {
+
+    //  Creates a health metric with specified type, value,
+    //  time and unit of measurement
+
+    public HealthMetric(String metricType, double value, String timestamp, String unit) {
+
+
+    }
+
+    // Updates the value of the metric 
+
+    public void updateValue(double value) {
+       
+    }
+
+    // Returns the list of past values and time stamps for a specified metric
+    public List<String> getHistoricalData() {
+        
+        return null;
+    }
+
+
+        
+}
