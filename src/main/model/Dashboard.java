b8574ccf46db51bbebb6f3a4066a4b3b6a438eda
Ella Lowe-Ng <ella.lowe-ng@smus.ca>
2024-10-10 23:55:59 -0700

    Added specification for the Dashbaord class

---
+++ src/main/model/Dashboard.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.util.List;
+
+public class Dashboard {
+
+    // REQUIRES: Patient must not be null
+    // MODIFIES: None
+    // EFFECTS: Displays the health metrics for the specified patient.
+
+    public void displayHealthMetrics(Patient patient) {
+       
+    }
+
+    // REQUIRES: user must not be null
+    // MODIFIES: None
+    // EFFECTS: Displays the health metrics for the specified user.
+
+    public void displayAlerts(User user) {
+        
+    }
+
+    // REQUIRES: None
+    // MODIFIES: None
+    // EFFECTS: Filters patients by there health status (Recovered, stable, discharged)
+
+    public List<Patient> filterPatientsByStatus(String status) {
+        
+        return null;
+    }
+}
