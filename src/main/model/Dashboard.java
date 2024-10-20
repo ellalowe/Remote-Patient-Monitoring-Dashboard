77079b9bb1714889120757761dd45f10ae1cb6be
Ella Lowe-Ng <ella.lowe-ng@smus.ca>
2024-10-20 01:12:13 -0700

    Redid the specification for the Dashboard class

--- src/main/model/Dashboard.java
+++ src/main/model/Dashboard.java
@@ -4,28 +4,57 @@ import java.util.List;
 
 public class Dashboard {
 
-    // REQUIRES: Patient must not be null
-    // MODIFIES: None
-    // EFFECTS: Displays the health metrics for the specified patient.
+    // EFFECTS: Has a list of patients intialized to be empty
 
-    public void displayHealthMetrics(Patient patient) {
+    public Dashboard () {
+
+    }
+
+    // REQUIRES: Patient must not be null and must not already be in the dashboard
+    // MODIFIES: The dashboard
+    // EFFECTS: Adds a list of all the new patients to the dashbaord
+
+    public void addPatients(List<Patient> newPatients) {
        
     }
 
-    // REQUIRES: user must not be null
+    // REQUIRES: Dashbaord must not be null
     // MODIFIES: None
-    // EFFECTS: Displays the health metrics for the specified user.
+    // EFFECTS: Returns a list of all patients currently in the dashboard. 
+    // If no patients exist, the method may return an empty list.
 
-    public void displayAlerts(User user) {
-        
-    }
-
-    // REQUIRES: None
-    // MODIFIES: None
-    // EFFECTS: Filters patients by there health status (Recovered, stable, discharged)
-
-    public List<Patient> filterPatientsByStatus(String status) {
-        
+    public List<Patient> listAllPatients() {
         return null;
+        
     }
+
+    // REQUIRES: Name should not be null and a patient should have a given name in the system
+    // MODIFIES: The list of patients in the dashboard
+    // EFFECTS: Removes the patient with the given name (case sensitive) from the dashboard. 
+    // Returns true if the patient was successfully removed, otherwise returns false.
+
+    public boolean removePatient(String name) {
+        
+        return false;
+    }
+
+
+    // REQUIRES: name must not be null, and there must be a patient with the given name in the system.
+    // MODIFIES: The status of the patient in the system.
+    // EFFECTS: Marks patient as critical. Returns true if successful and false otherwise 
+  
+
+    public boolean markPatientAsCritical(String name) {
+
+        return false;
+
+
+    }
+
+    public void add(List<Patient> newPatients) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+	
 }
