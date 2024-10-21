028517509905bff601639048641d55e49bec7ea8
Ella Lowe-Ng <ella.lowe-ng@smus.ca>
2024-10-21 00:53:23 -0700

    Added specification for the HealthcareProvider class

---
+++ src/main/model/HealthcareProvider.java
@@ -0,0 +1,21 @@
+package model;
+
+// Represents a healthcare provider with a name.
+
+public class HealthcareProvider {
+
+
+    // Constructs a new HealthcareProvider with the given name.
+    public HealthcareProvider(String name) {
+
+
+    }
+
+    public String getName() {
+
+        return null;
+    }
+
+
+
+}
